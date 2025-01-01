93d94aa7355fb51f889c0fd7157710993af962ef
sahlminkok <sahlminkok7@gmail.com>
2025-01-01 19:27:51 +0100

    Handle loading, error, and ready status to display data in main component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import { useEffect, useReducer } from "react";
 import Header from "./Header";
 import Main from "./Main";
+import Loader from "./Loader";
+import Error from "./Error";
+import StartScreen from "./StartScreen";
 
 const initialState = {
   questions: [],
 
   // 'loading', 'error', 'ready', 'active', 'finished'
-  state: "loading",
+  status: "loading",
 };
 
 function reducer(state, action) {
@@ -15,7 +18,7 @@ function reducer(state, action) {
       return {
         ...state,
         questions: action.payload,
-        state: "ready",
+        status: "ready",
       };
     case "dataFailed":
       return { ...state, status: "error" };
@@ -25,7 +28,9 @@ function reducer(state, action) {
 }
 
 function App() {
-  const [state, dispatch] = useReducer(reducer, initialState);
+  const [{ questions, status }, dispatch] = useReducer(reducer, initialState);
+
+  const numQuestions = questions.length;
 
   useEffect(() => {
     fetch("http://localhost:8000/questions")
@@ -39,8 +44,9 @@ function App() {
       <Header />
 
       <Main>
-        <p>1/15</p>
-        <p>Question?</p>
+        {status === "loading" && <Loader />}
+        {status === "error" && <Error />}
+        {status === "ready" && <StartScreen numQuestions={numQuestions} />}
       </Main>
     </div>
   );
