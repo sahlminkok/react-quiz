37351c66b818bf79bfe36335923d3c64d54d60c5
sahlminkok <sahlminkok7@gmail.com>
2025-01-01 23:50:50 +0100

    Move components to component folder

--- src/components/App.js
+++ src/components/App.js
@@ -3,7 +3,7 @@ import Header from "./Header";
 import Main from "./Main";
 import Loader from "./Loader";
 import Error from "./Error";
-import StartScreen from "./StartScreen";
+import StartScreen from "../StartScreen";
 import Question from "./Question";
 
 const initialState = {
@@ -11,6 +11,7 @@ const initialState = {
 
   // 'loading', 'error', 'ready', 'active', 'finished'
   status: "loading",
+  index: 0,
 };
 
 function reducer(state, action) {
@@ -31,7 +32,10 @@ function reducer(state, action) {
 }
 
 function App() {
-  const [{ questions, status }, dispatch] = useReducer(reducer, initialState);
+  const [{ questions, status, index }, dispatch] = useReducer(
+    reducer,
+    initialState
+  );
 
   const numQuestions = questions.length;
 
@@ -52,7 +56,7 @@ function App() {
         {status === "ready" && (
           <StartScreen numQuestions={numQuestions} dispatch={dispatch} />
         )}
-        {status === "active" && <Question />}
+        {status === "active" && <Question question={questions[index]} />}
       </Main>
     </div>
   );
---
+++ src/components/Question.js
@@ -0,0 +1,9 @@
+function Question({ question }) {
+  return (
+    <div>
+      <h4>{question.question}</h4>
+    </div>
+  );
+}
+
+export default Question;
