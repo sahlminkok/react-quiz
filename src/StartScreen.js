5198ffc629244fb88bcd29e49b9e299236d22316
sahlminkok <sahlminkok7@gmail.com>
2025-01-01 19:39:32 +0100

    Add on click handler to dispatch action type start

--- src/StartScreen.js
+++ src/StartScreen.js
@@ -1,9 +1,14 @@
-function StartScreen({ numQuestions }) {
+function StartScreen({ numQuestions, dispatch }) {
   return (
     <div className="start">
       <h2>Welcome to The React Quiz!</h2>
       <h3>{numQuestions} questions to test your React mastery</h3>
-      <button className="btn btn-ui">let's start</button>
+      <button
+        className="btn btn-ui"
+        onClick={() => dispatch({ type: "start" })}
+      >
+        let's start
+      </button>
     </div>
   );
 }
