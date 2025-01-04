5e904518c0495595930bba73f88c7473c1a8b5b8
sahlminkok <sahlminkok7@gmail.com>
2025-01-04 09:54:30 +0100

    Create options component

---
+++ src/components/Options.js
@@ -0,0 +1,11 @@
+function Options({ options }) {
+  return (
+    <div className="options">
+      {options.map((option) => (
+        <button className="btn btn-option">{option}</button>
+      ))}
+    </div>
+  );
+}
+
+export default Options;
