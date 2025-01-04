d9bc77a07b2bb172d84a696e6b2a1b7e9147d7e1
sahlminkok <sahlminkok7@gmail.com>
2025-01-04 09:55:53 +0100

    Import options component in question and pass in options as a prop

--- src/components/Question.js
+++ src/components/Question.js
@@ -1,7 +1,11 @@
+import Options from "./Options";
+
 function Question({ question }) {
   return (
     <div>
       <h4>{question.question}</h4>
+
+      <Options options={question.options} />
     </div>
   );
 }
