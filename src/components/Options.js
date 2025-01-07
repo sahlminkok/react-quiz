821395e8568aa18b34bb05bdb2132d046d818e3d
sahlminkok <sahlminkok7@gmail.com>
2025-01-07 13:30:59 +0100

    Add on click handler to dispatch action with type newAnswer

--- src/components/Options.js
+++ src/components/Options.js
@@ -1,8 +1,23 @@
-function Options({ options }) {
+function Options({ question, answer, dispatch }) {
+  const hasAnswered = answer !== null;
+
   return (
     <div className="options">
-      {options.map((option) => (
-        <button className="btn btn-option">{option}</button>
+      {question.options.map((option, index) => (
+        <button
+          className={`btn btn-option ${index === answer ? "answer" : ""} ${
+            hasAnswered
+              ? index === question.correctOption
+                ? "correct"
+                : "wrong"
+              : ""
+          }`}
+          key={option}
+          disabled={hasAnswered}
+          onClick={() => dispatch({ type: "newAnswer", payload: index })}
+        >
+          {option}
+        </button>
       ))}
     </div>
   );
