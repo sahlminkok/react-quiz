91a3fc34521ef759d1f34e6fbe5ab2448f3395c2
sahlminkok <sahlminkok7@gmail.com>
2025-01-08 12:22:01 +0100

    Add points to initial state, and update it in reducer function

--- src/components/App.js
+++ src/components/App.js
@@ -13,6 +13,7 @@ const initialState = {
   status: "loading",
   index: 0,
   answer: null,
+  points: 0,
 };
 
 function reducer(state, action) {
@@ -28,7 +29,16 @@ function reducer(state, action) {
     case "start":
       return { ...state, status: "active" };
     case "newAnswer":
-      return { ...state, answer: action.payload };
+      const question = state.questions.at(state.index);
+
+      return {
+        ...state,
+        answer: action.payload,
+        points:
+          action.payload === question.correctOption
+            ? state.points + question.points
+            : state.points,
+      };
     default:
       throw new Error("Unknown action");
   }
