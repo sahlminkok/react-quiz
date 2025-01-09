612b1a395f377ee41b9bdc43020a3e361843a3da
sahlminkok <sahlminkok7@gmail.com>
2025-01-09 10:22:44 +0100

    Add a restart action to reducer

--- src/components/App.js
+++ src/components/App.js
@@ -52,6 +52,8 @@ function reducer(state, action) {
         highscore:
           state.points > state.highscore ? state.points : state.highscore,
       };
+    case "restart":
+      return { ...initialState, questions: state.questions, status: "ready" };
     default:
       throw new Error("Unknown action");
   }
@@ -110,6 +112,7 @@ function App() {
             points={points}
             maxPossiblePoints={maxPossiblePoints}
             highscore={highscore}
+            dispatch={dispatch}
           />
         )}
       </Main>
