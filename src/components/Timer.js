53781f250f24bd97f1b6be75c342300c6dc56d55
sahlminkok <sahlminkok7@gmail.com>
2025-01-09 12:42:21 +0100

    Calculate minutes and seconds and display accordingly

--- src/components/Timer.js
+++ src/components/Timer.js
@@ -1,13 +1,20 @@
 import { useEffect } from "react";
 
 function Timer({ dispatch, secondsRemaining }) {
+  const min = Math.floor(secondsRemaining / 60);
+  const seconds = secondsRemaining % 60;
+
   useEffect(() => {
     const id = setInterval(() => dispatch({ type: "tick" }), 1000);
 
     return () => clearInterval(id);
   }, [dispatch]);
 
-  return <div className="timer">{secondsRemaining}</div>;
+  return (
+    <div className="timer">
+      {min < 10 ? `0${min}` : min}:{seconds < 10 ? `0${seconds}` : seconds}
+    </div>
+  );
 }
 
 export default Timer;
